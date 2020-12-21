73bf6e7dd35f0096105a9f99ab9fd9e1a7064778
user0001 <user0001@jec.ac.jp>
2020-12-21 19:01:06 +0900

    initial commit

---
+++ app/src/main/java/jp/ac/jec/jz/bluetoothsample2020/DeviceListActivity.java
@@ -0,0 +1,14 @@
+package jp.ac.jec.jz.bluetoothsample2020;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DeviceListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_device_list);
+    }
+}
